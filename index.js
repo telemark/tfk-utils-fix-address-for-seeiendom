030832892b4cc756760e21adf0062bfc91b00fb5
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-12-16 21:45:47 +0100

    Adds regex

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
 'use strict'
 
 function fixAddress (address) {
-  var splittedAddressOnComma = address.split(',')
-  var splittedAddress = splittedAddressOnComma[0].split(' ')
-  var fixedAddress = []
-  var prevItem = ''
-  var thisItem = ''
-  splittedAddress.forEach(function (item) {
-    thisItem = item
-    if (thisItem.length === 1 && parseInt(prevItem) > 0) {
-      fixedAddress.pop(fixedAddress.length - 1)
-      fixedAddress.push(prevItem + thisItem)
-      prevItem = ''
-    } else {
-      prevItem = thisItem
-    }
-    if (prevItem != '') {
-      fixedAddress.push(prevItem)
-    }
-  })
-  return fixedAddress.join(' ') + ',' + splittedAddressOnComma[1]
+  var matched = address.match(/\d\s\w,/g)[0]
+  var fixed = matched.replace(' ', '')
+  return address.replace(matched, fixed)
 }
 
 module.exports = fixAddress
